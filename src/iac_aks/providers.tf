c0239f38529463abacda015620c1e6c2866bd6a7
Gerasimos Alexiou <geralexgr@gmail.com>
2024-03-31 20:34:19 +0300

    push

---
+++ src/iac_aks/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=1.7"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.9"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
