2302d12fe740b492c12306a62c61b61c832d21d3
Gerasimos Alexiou <geralexgr@gmail.com>
2024-04-04 19:00:55 +0300

    change provider version

--- src/iac_devops/main.tf
+++ src/iac_devops/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azuredevops = {
       source = "microsoft/azuredevops"
-      version = ">=0.1.6"
+      version = ">=1.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
